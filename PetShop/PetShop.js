b060b527c1478ff031a210204247a6c718bb536f
AliSe2416dJS <alina.s.info@gmail.com>
2017-07-04 11:20:48 +0300

    Pattern module added

--- PetShop/PetShop.js
+++ PetShop/PetShop.js
@@ -44,3 +44,44 @@ class PetShop {
         return whiteAndFluffyList;
     }
 }
+
+
+let Shop1 = (function () {
+    let petList = [];
+
+        petList.push(new Cat('Kitty', true, 'White', 500));
+        petList.push(new Cat('Tom', false, 'Grey', 150));
+        petList.push(new Cat('Barsik', false, 'Striped', 400));
+        petList.push(new Cat('Murzik',true, 'Red', 600));
+        petList.push(new Dog('Bingo', 'White', 400));
+        petList.push(new Dog('Bob', 'Black', 300));
+        petList.push(new Dog('Patrick', 'Red', 650));
+        petList.push(new Hamster(false, 'White', 550));
+        petList.push(new Hamster(true, 'Black', 160));
+        petList.push(new Hamster(false, 'Spotted', 250));
+
+    function _getCats () {
+        return petList.filter((pet) => pet instanceof Cat);
+    }
+
+    function _getExpensivePets () {
+        let expensivePetList = [],
+            averagePrice = 0;
+            
+        petList.forEach((pet) => averagePrice += pet.price);
+        averagePrice /= petList.length;
+        expensivePetList = petList.filter((pet) => pet.price > averagePrice);
+
+        return expensivePetList;
+    }
+
+    function _getWhiteOrFluffy () {
+        return petList.filter((pet) => pet.color === 'White' || pet.isFluffy);
+    }
+
+    return {
+        getCats: _getCats,
+        getExpensivePets: _getExpensivePets,
+        getWhiteOrFluffy: _getWhiteOrFluffy
+    };
+})();
--- PetShop/main.js
+++ PetShop/main.js
@@ -3,10 +3,14 @@
 document.addEventListener('DOMContentLoaded', init, false);
 
 function init () {
-    let shop = new PetShop(),
+    let //shop = new PetShop(),
         view = new PetShopView();
 
-    view.showPets('Cats', shop.cats);
-    view.showPets('Expensive pets', shop.expensivePets);
-    view.showPets('White or fluffy pets', shop.whiteOrFluffy);
+//    view.showPets('Cats', shop.cats);
+//    view.showPets('Expensive pets', shop.expensivePets);
+//    view.showPets('White or fluffy pets', shop.whiteOrFluffy);
+
+    view.showPets('Cats', Shop1.getCats());
+    view.showPets('Expensive pets', Shop1.getExpensivePets());
+    view.showPets('White or fluffy pets', Shop1.getWhiteOrFluffy());   
 }
