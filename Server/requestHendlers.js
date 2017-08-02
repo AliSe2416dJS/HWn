20a7a986303beb8dadc0e3070e91ed53c011bcd9
AlinaSed <alina.sedletska@gmail.com>
2017-08-02 11:40:20 +0300

    Server modified: current time and date added

--- Server/requestHendlers.js
+++ Server/requestHendlers.js
@@ -2,37 +2,23 @@
 
 let fs = require('fs');
 
-/*function start (response) {
-    var body = '<html>'+
-    '<head>'+
-    '<meta http-equiv="Content-Type" content="text/html; '+
-    'charset=UTF-8" />'+
-    '</head>'+
-    '<body>'+
-    '<form action="/upload" method="post">'+
-    '<textarea name="text" rows="20" cols="60"></textarea>'+
-    '<input type="submit" value="Submit text" />'+
-    '</form>'+
-    '</body>'+
-    '</html>';
-
-    console.log('request handler "start" was called');
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(body);
-    response.end();
-}*/
-
 function getTime (response) {
+    let now = new Date(),
+        time = `${now.getHours()}:${now.getMinutes()}`;
+
     console.log('request handler "time" was called');
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write('11:10');
+    response.write(time);
     response.end();
 }
 
 function getDate (response) {
+    let now = new Date(),
+        date = `${now.getDate()}.${now.getMonth()+1}.${now.getFullYear()}`;
+
     console.log('request handler "time" was called');
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write('02.08.2017');
+    response.write(date);
     response.end();
 }
 
@@ -50,14 +36,8 @@ function uploadIndex (response) {
             response.end();
         }
   });
-
-
-  /*  response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write('hello upload!!!');
-    response.end();*/
 }
 
-//exports.start = start;
 exports.getTime = getTime;
 exports.getDate = getDate;
 exports.uploadIndex = uploadIndex;
